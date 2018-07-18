a71b50669390b3bdb6c1bfa0c5d25126225efe76
Michael Sklepowich <michaelsklepowich@Michaels-MBP-3.localdomain>
2018-07-18 11:46:21 -0700

    creating initial notes component as a form

--- src/components/readme/readme.js
+++ src/components/readme/readme.js
@@ -2,15 +2,27 @@ import './readme.css';
 
 import React from 'react';
 import superagent from 'superagent';
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown';
+import Notes from '../notes/notes.js';
+import * as api from '../../lib/api.js';
 
 export default class Readme extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {content: ''}
+        this.submitNotes = this.submitNotes.bind(this);
     }
 
+    // need to set up route on backend to handle note submissions
+    async submitNotes(input){
+        let payload = {
+            endpoint:'notes',
+            body:input
+        }
+        let solution = await api.post(payload);
+        console.log('api routing', {solution});
+      }
     //Changed from componentWillUpdate after spinner was setup
     async componentWillMount(prevProps, prevState) {
         let url = this.props.readmeDoc;
@@ -18,13 +30,14 @@ export default class Readme extends React.Component {
             let data = await superagent.get(url);
             let content = atob(data.body.content);
             this.setState({ content });
-        } 
+        }
     }
 
     render() {
         return (
             <div className="readme">
-                <ReactMarkdown source={this.state.content} />
+              <Notes submitNotes={this.submitNotes}/>
+              <ReactMarkdown source={this.state.content} />
             </div>
         )
     }
